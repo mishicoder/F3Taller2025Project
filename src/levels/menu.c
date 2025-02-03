4869ea12c93be4ea9ae87393d71c5ffabdfeea52
Mishicoder <daedgamesperu@gmail.com>
2025-02-03 10:50:09 -0500

    create

--- src/levels/menu.c
+++ src/levels/menu.c
@@ -4,11 +4,7 @@ void Menu_Run(Game* game, GameLevel* level, ecs_entity_t entity)
 {
 	if (level->isComponentsAdded == 0)
 	{
-
-		LoadSprite(game, "assets/window_icon.png", "test");
-
 		LoadTilsetsPack(game, "assets/tilesets/pack_0.tspack", "base");
-		LoadTileMap(game, "assets/maps/farm_ext.tmx", "base", "test");
 		LoadTileMap(game, "assets/maps/farmhouse.tmx", "base", "player_house");
 
 		level->isComponentsAdded = 1;
--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <raylib.h>
+#include "levels/menu.h"
 #include "core/utilities.h"
 
 float speed = 2.0f;
@@ -21,6 +22,7 @@ int main()
 	SetTargetFPS(60);
 
 	Rectangle rectA = { 20, 20, 50, 50 };
+	Rectangle rectB = { 200, 80, 400, 80 };
 	Circle circle = { 200, 200, 40 };
 
 	while (!WindowShouldClose())
@@ -43,11 +45,13 @@ int main()
 		}
 
 		ResolveRectangleCircleCollision(&rectA, circle);
+		ResolveRectRectCollision(&rectA, rectB);
 
 		BeginDrawing();
 		ClearBackground((Color) { 33, 33, 33, 255 });
 
 		DrawRectangle(rectA.x, rectA.y, rectA.width, rectA.height, BLUE);
+		DrawRectangle(rectB.x, rectB.y, rectB.width, rectB.height, RED);
 		DrawCircle(circle.centerX, circle.centerY, circle.radius, WHITE);
 
 		EndDrawing();
@@ -64,7 +68,7 @@ int main()
 	config.colorBackground = (Color){ 20, 23, 21, 255 };
 
 	Game game;
-	InitGame(&game, config);
+	InitGame(&game, config, NULL);
 	SetGameWindowIcon(&game, "assets/game_icon.png");
 
 	//AddLevel(&game, "test", 0, 0, 0, TL_Run);
